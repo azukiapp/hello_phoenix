abdbde5ace853fad572b83d3395609a8b626cc6e
Gullit Miranda <gullitmiranda@gmail.com>
2015-06-06 19:32:57 -0300

    Updating Azkfile.js to Phoenix template

--- Azkfile.js
+++ Azkfile.js
@@ -11,16 +11,25 @@ systems({
     image: {"docker": "azukiapp/elixir"},
     // Steps to execute before running instances
     provision: [
-      "mix do deps.get, mix compile"
+      // "mix local.hex --force", // update mix
+      "npm install",
+      "mix do deps.get, compile",
     ],
     workdir: '/azk/#{manifest.dir}',
     shell: "/bin/bash",
-    command: "mix run --no-halt",
+    // Phoenix Framework
+    command: "mix phoenix.server --no-deps-check",
     wait: {"retry": 20, "timeout": 1000},
     mounts: {
-      "/azk/#{manifest.dir}"       : sync("."),
-      "/azk/#{manifest.dir}/_build": persistent("#{system.name}/_build"),
-      "/azk/#{manifest.dir}/.hex"  : persistent("#{system.name}/.hex"),
+      "/azk/#{manifest.dir}"             : sync("."),
+      // Dependêncies
+      "/azk/#{manifest.dir}/deps"           : persistent("#{manifest.dir}/deps"),
+      "/azk/#{manifest.dir}/.hex"           : persistent("#{manifest.dir}/.hex"),
+      "/azk/#{manifest.dir}/node_modules"   : persistent("#{manifest.dir}/node_modules"),
+      // Builds
+      "/azk/#{manifest.dir}/_build"         : persistent("#{manifest.dir}/_build"),
+      "/azk/#{manifest.dir}/priv/static/js" : persistent("#{manifest.dir}/priv/static/js"),
+      "/azk/#{manifest.dir}/priv/static/css": persistent("#{manifest.dir}/priv/static/css"),
     },
     scalable: {"default": 1},
     http: {
